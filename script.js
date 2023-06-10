cc32dce4073322033c692406dcffeb159dd9512b
ElamaranDev <marandev007@gmail.com>
2023-06-10 19:00:17 +0530

    final commit

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const del = document.querySelector('.delete-btn');
 const taskName = document.querySelector('.task-name');
 const dueDate = document.querySelector('.task-date');
 
+const date = new Date();
+var day = String(date.getDate()).padStart(2, '0');
+var month = String(date.getMonth() + 1).padStart(2, '0');
+var year = date.getFullYear();
+var currentDate = `${day}-${month}-${year}`;
+
 
 let task = [];
 
@@ -26,9 +32,9 @@ function renderToDo(){
     for(let i=0; i < task.length; i++){
         const toDo = task[i];
         const { taskname } = toDo;
-        const { taskdate } = toDo;
+        var { taskdate } = toDo;
         const html = `
-        <div class="task">
+        <div class="task" data-index="${i}">
             <span id="task-num" class="task-num">
             ${i + 1}
             </span>
@@ -42,7 +48,11 @@ function renderToDo(){
                 ${ taskdate }
             </span>
             <div class="actions">
-                <button class="edit-btn" id="edit">Edit</button>
+                <button 
+                class="edit-btn" 
+                id="edit">
+                        Edit
+                </button>
                 <button
                 onclick="
                 task.splice(${i}, 1);
@@ -53,20 +63,42 @@ function renderToDo(){
             </div> 
         </div>`;
         toDoList += html;
+    }
         tasks.innerHTML = toDoList;
-    }
-    if (task.length === 0) {
-        tasks.innerHTML = '';
-    }
+        if (task.length === 0) {
+            tasks.innerHTML = '';
+        }
+
+        const editButtons = document.querySelectorAll('.edit-btn');
+        editButtons.forEach((editButton, i) => {
+            editButton.addEventListener('mouseup', () => {
+                editTask(i);
+            });
+        });
 }
+
 function addtoList(){
     const inputText = document.querySelector('.task-input');
-    const inputDate = document.querySelector('.date-input');
-    const inputTask = { taskname:inputText.value, taskdate:inputDate.value };
+    const inputDate = document.querySelector('.date-input').value;
+    
+    let inDate = currentDate;
+
+    if (inputDate !== '') {
+        var newInputDate = new Date(inputDate);
+        var inDay = String(newInputDate.getDate()).padStart(2, '0');
+        var inMonth = String(newInputDate.getMonth() + 1).padStart(2, '0');
+        var inYear = newInputDate.getFullYear();
+        inDate = `${inDay}-${inMonth}-${inYear}`;
+    }
+
+    const inputTask = { taskname:inputText.value, taskdate:inDate };
+
     task.push(inputTask);
+    console.log(task);
     saveTaskList();
     renderToDo();
 }
+
 function saveTaskList(){
     localStorage.setItem('Task', JSON.stringify(task));
 }
@@ -78,5 +110,26 @@ function retriveTaskList(){
     }
     else{
         task = [];
+    }
+}
+
+function editTask(i) {
+    const edit = document.querySelector(`[data-index="${i}"] .edit-btn`);
+    const taskInput = document.querySelector(`[data-index="${i}"] .task-name`);
+    var previousValue = '';
+    if(edit.innerHTML === 'Edit'){
+        edit.innerHTML = 'Save';
+        taskInput.removeAttribute('readonly');
+        taskInput.focus();
+        previousValue = taskInput.value;
+    }
+    else{
+        edit.innerHTML = 'Edit';
+        taskInput.setAttribute('readonly', 'readonly');
+        if(taskInput.value !== previousValue) 
+        {
+            task[i].taskname = taskInput.value;
+            saveTaskList();
+        }
     }
 }
